2fe72b49b05c6f535713062c91dbd957b11576ba
huangweii <huangweii@chanjet.com>
2018-06-25 15:37:38 +0800

    更新pom,生成可执行的jar 包

--- src/main/java/com/kunlunsoft/sms/Main.java
+++ src/main/java/com/kunlunsoft/sms/Main.java
@@ -2,12 +2,16 @@ package com.kunlunsoft.sms;
 
 import com.kunlunsoft.sms.dto.SmsParam;
 import com.kunlunsoft.sms.util.SmsSendUtil;
+import com.string.widget.util.ValueWidget;
 
 /***
  * z主程序
  */
 public class Main {
     public static void main(String[] args) {
+        if (ValueWidget.isNullOrEmpty(args)) {
+            args = new String[]{"18701670186", "服务器down 了"};
+        }
         String mobile = args[0];
         String message = args[1];
         SmsParam smsParam = SmsSendUtil.getSmsConf();
